3082b1290d45faf8846fc9c9fe6930c2994c8404
ropfeg <ropf.eg@gmail.com>
2021-02-25 09:07:36 +0200

    Revert "first_upload_25_feb"
    This reverts commit fa3e162e979dcdb270d3a134cbb15433ccd5d23a.

--- lib/models/enums.dart
+++
@@ -1,2 +0,0 @@
-enum MenuState { home, fav, profile, notselected }
-enum category { Cat, Dog, Service, Best_Selling }
--- lib/redux/actions/inloading_action.dart
+++
@@ -1,9 +0,0 @@
-import 'package:pawpoint/models/inloading.dart';
-
-class UpdateInloading {
-  final InLoading _inLoading;
-
-  InLoading get inloading => this._inLoading;
-
-  UpdateInloading(this._inLoading);
-}
